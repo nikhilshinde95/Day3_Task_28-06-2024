f97101045e46d598866fb5e2388dc8da482fbb89
Nikhil Shinde <nikhil.shinde@bnt-soft.com>
2024-07-02 23:31:20 +0530

    Update CategoryRepository.java

--- TestManagement/src/main/java/com/org/dao/CategoryRepository.java
+++ TestManagement/src/main/java/com/org/dao/CategoryRepository.java
@@ -1,8 +1,17 @@
 package com.org.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.org.entities.Category;
+import com.org.entities.Subcategory;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	 @Query("SELECT s FROM Category s WHERE s.name = :name")
+	    List<Category> findByName(@Param("name") String name);
+
 }
