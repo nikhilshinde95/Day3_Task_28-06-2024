f758a532965d81bc735e08619c50118f4c2324d3
Nikhil Shinde <nikhil.shinde@bnt-soft.com>
2024-07-03 12:23:33 +0530

    Update SubcategoryServiceImpl.java

--- TestManagement/src/main/java/com/org/services/SubcategoryServiceImpl.java
+++ TestManagement/src/main/java/com/org/services/SubcategoryServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +16,20 @@ import com.org.entities.Subcategory;
 
 @Service
 public class SubcategoryServiceImpl implements SubcategoryService {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(SubcategoryServiceImpl.class);
 
 	@Autowired
 	private SubcategoryRepository subcategoryRepository;
+	
 
 	@Override
 	public List<Subcategory> getAllSubcategories() {
-//		return subcategoryRepository.findAll();
+		log.info("Fetching All Subcategories...");
 		List<Subcategory> results = subcategoryRepository.findAll()
 		        .stream()
-		        .filter(Objects::nonNull) // Filter out null values
+		        .filter(Objects::nonNull) // Filter out null values by using stream API
 		        .collect(Collectors.toList());
 		
 		return results;
@@ -30,27 +37,44 @@ public class SubcategoryServiceImpl implements SubcategoryService {
 
 	@Override
 	public Subcategory getSubcategoryById(Long id) {
+		
+		log.info("Fetching Subcategory By {} ID ",id);
+		
 		Optional<Subcategory> subcategory = subcategoryRepository.findById(id);
+		
 		return subcategory.orElseThrow(() -> new RuntimeException("Subcategory not found"));
 	}
 
 	@Override
 	public Subcategory createSubcategory(Subcategory subcategory) {
-		return subcategoryRepository.save(subcategory);
+		log.info("Adding New Subcategory in Database...");
+		Subcategory saveSubcategories = subcategoryRepository.save(subcategory);
+		log.info("New Subcategory is Added Successfully..");
+		return saveSubcategories;
 	}
 
 	@Override
 	public Subcategory updateSubcategory(Subcategory subcategory) {
-		return subcategoryRepository.save(subcategory);
+		
+		log.info("Updating Existing Subcategories..");
+		Subcategory updateSubcategories = subcategoryRepository.save(subcategory);
+		log.info("Subcategory is Updated Successfully...");
+		return updateSubcategories;
 	}
 
 	@Override
 	public boolean deleteSubcategory(Long id) {
+		
+		log.info("Deleting Subcategories by using Id {} ",id);
 		Optional<Subcategory> subcategory = subcategoryRepository.findById(id);
+		
 		if (subcategory.isPresent()) {
+			
+			log.debug("Subcategory with Id {} is Deleted Successfully..",id);
 			subcategoryRepository.delete(subcategory.get());
 			return true;
 		} else {
+			log.error("Subcategory is Not Found.");
 			throw new RuntimeException("Subcategory not found");
 		}
 	}
