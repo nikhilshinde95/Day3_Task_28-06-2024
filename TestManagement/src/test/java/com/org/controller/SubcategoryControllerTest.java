1781082c29fc761afc48690be2666d0d94579a9c
Nikhil Shinde <nikhil.shinde@bnt-soft.com>
2024-07-07 16:42:28 +0530

    Some changes in SubcategoryControllerTest.java

--- TestManagement/src/test/java/com/org/controller/SubcategoryControllerTest.java
+++ TestManagement/src/test/java/com/org/controller/SubcategoryControllerTest.java
@@ -39,7 +39,6 @@ public class SubcategoryControllerTest {
 
     @BeforeEach
     public void setUp() {
-    	
         category1 = new Category(1L, "Java", "Core Java category");
         subcategory1 = new Subcategory(1L, category1, "Spring MVC", "Spring MVC subcategory");
         subcategory2 = new Subcategory(2L, category1, "Hibernate", "Hibernate subcategory");
@@ -48,13 +47,18 @@ public class SubcategoryControllerTest {
     @Test
     void testGetAllSubcategories() {
     	
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory1);
+    	ResponseEntity<Object> addSubcategory1 = subcategoryController.createSubcategory(subcategory1);
+    	
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory2);
+    	ResponseEntity<Object> addSubcategory2 = subcategoryController.createSubcategory(subcategory2);
+    	
         List<Subcategory> list = new ArrayList<>();
         list.add(subcategory1);
         list.add(subcategory2);
  
         when(subcategoryService.getAllSubcategories()).thenReturn(list);
         List<Subcategory> returnedSubcategories = subcategoryController.getAllSubcategories();
-        
         assertEquals(2, returnedSubcategories.size(), "Expected 2 subcategories");
         assertEquals("Spring MVC", returnedSubcategories.get(0).getName(), "First subcategory name should be Spring MVC");
         assertEquals("Hibernate", returnedSubcategories.get(1).getName(), "Second subcategory name should be Hibernate");
@@ -62,91 +66,47 @@ public class SubcategoryControllerTest {
 
     @Test
     void testGetSubcategoryByIdValid() {
-        Long subcategoryId = 1L;
+        
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory1);
+    	ResponseEntity<Object> addSubcategory = subcategoryController.createSubcategory(subcategory1);
 
-        when(subcategoryService.getSubcategoryById(subcategoryId)).thenReturn(subcategory1);
-
-        ResponseEntity<Subcategory> responseEntity = subcategoryController.getSubcategoryById(subcategoryId);
-
+        when(subcategoryService.getSubcategoryById(anyLong())).thenReturn(subcategory1);
+        ResponseEntity<Subcategory> responseEntity = subcategoryController.getSubcategoryById(1l);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode(), "Expected status OK");
         assertEquals("Spring MVC", responseEntity.getBody().getName(), "Subcategory name should be Spring MVC");
     }
 
     @Test
-    void testGetSubcategoryByIdNotFound() {
-        Long subcategoryId = 99L;
-
-        when(subcategoryService.getSubcategoryById(subcategoryId)).thenReturn(null);
-
-        ResponseEntity<Subcategory> responseEntity = subcategoryController.getSubcategoryById(subcategoryId);
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Expected status Not Found");
-    }
-
-    @Test
     void testCreateSubcategory() {
-        Subcategory newSubcategory = new Subcategory(null, category1, "Spring Boot", "Spring Boot subcategory");
-        Subcategory savedSubcategory = new Subcategory(3L, category1, "Spring Boot", "Spring Boot subcategory");
-        
-        when(categoryService.getCategoryById(category1.getId())).thenReturn(category1);
-        when(subcategoryService.createSubcategory(newSubcategory)).thenReturn(savedSubcategory);
-
-        ResponseEntity<Object> responseEntity = subcategoryController.createSubcategory(newSubcategory);
-
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory1);
+    	ResponseEntity<Object> addSubcategory = subcategoryController.createSubcategory(subcategory1);
+        assertEquals(HttpStatus.CREATED, addSubcategory.getStatusCode());
 
     }
 
-    @Test
-    void testCreateSubcategoryCategoryNotFound() {
-        Subcategory newSubcategory = new Subcategory(null, category1, "Spring Boot", "Spring Boot subcategory");
-        
-        when(categoryService.getCategoryById(category1.getId())).thenReturn(null);
-        ResponseEntity<Object> responseEntity = subcategoryController.createSubcategory(newSubcategory);
-
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
-    }
 
     @Test
     void testUpdateSubcategory() {
-    	Long subcategoryId=1L;
-        Subcategory updatedSubcategory = new Subcategory(subcategoryId, category1, "Spring MVC Updated", "Updated Spring MVC subcategory");
-        when(categoryService.getCategoryById(category1.getId())).thenReturn(category1);
+    	
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory1);
+    	Subcategory addSubcategory = subcategoryService.createSubcategory(subcategory1);
+    	
+        Subcategory updatedSubcategory = new Subcategory(1L, category1, "Spring MVC Updated", "Updated Spring MVC subcategory");
         when(subcategoryService.updateSubcategory(updatedSubcategory)).thenReturn(updatedSubcategory);
-
-        ResponseEntity<Subcategory> responseEntity = subcategoryController.updateSubcategory(subcategoryId, updatedSubcategory);
-
+        ResponseEntity<Subcategory> responseEntity = subcategoryController.updateSubcategory(1L, updatedSubcategory);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode(), "Expected status OK");
         assertEquals("Spring MVC Updated", responseEntity.getBody().getName(), "Subcategory name should be Spring MVC Updated");
     }
 
     @Test
-    void testUpdateSubcategoryCategoryNotFound() {
-        Long subcategoryId = 1L;
-        Subcategory updatedSubcategory = new Subcategory(subcategoryId, category1, "Spring MVC Updated", "Updated Spring MVC subcategory");
-
-        when(categoryService.getCategoryById(category1.getId())).thenReturn(null);
-
-        ResponseEntity<Subcategory> responseEntity = subcategoryController.updateSubcategory(subcategoryId, updatedSubcategory);
-
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Expected status Not Found");
-    }
-
-    @Test
     void testDeleteSubcategory() {
-        Long subcategoryId = 1L;
+    	
+    	when(subcategoryService.createSubcategory(any(Subcategory.class))).thenReturn(subcategory1);
+    	ResponseEntity<Object> addSubcategory = subcategoryController.createSubcategory(subcategory1);
 
-        when(subcategoryService.deleteSubcategory(subcategoryId)).thenReturn(true);
-        ResponseEntity<HttpStatus> responseEntity = subcategoryController.deleteSubcategory(subcategoryId);
-
+        when(subcategoryService.deleteSubcategory(anyLong())).thenReturn(true);
+        ResponseEntity<HttpStatus> responseEntity = subcategoryController.deleteSubcategory(1L);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode(), "Expected status OK");
     }
 
-    @Test
-    void testDeleteSubcategoryNotFound() {
-        Long subcategoryId = 99L;
-        when(subcategoryService.deleteSubcategory(subcategoryId)).thenReturn(false);
-        ResponseEntity<HttpStatus> responseEntity = subcategoryController.deleteSubcategory(subcategoryId);
-
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Expected status Not Found");
-    }
 }
