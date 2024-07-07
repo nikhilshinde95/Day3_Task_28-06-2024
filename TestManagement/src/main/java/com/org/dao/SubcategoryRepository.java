90b19f9eb8fbe731533ff6424c6da71663ea1314
Nikhil Shinde <nikhil.shinde@bnt-soft.com>
2024-07-07 16:17:50 +0530

    Update SubcategoryRepository.java

--- TestManagement/src/main/java/com/org/dao/SubcategoryRepository.java
+++ TestManagement/src/main/java/com/org/dao/SubcategoryRepository.java
@@ -6,16 +6,17 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import com.org.entities.Category;
 import com.org.entities.Subcategory;
 
+@Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, Long> {
 
     @Query("SELECT s FROM Subcategory s WHERE s.name = :name")
     List<Subcategory> findByName(@Param("name") String name);
 
     @Query("SELECT s FROM Subcategory s WHERE s.name = :name and s.description = :description")
-	 Optional<Subcategory> findByNameandFindByDescription(@Param("name") String name, @Param("description") String description);
-    // You can add more custom queries as needed
+	 Optional<Subcategory> findByNameandDescription(@Param("name") String name, @Param("description") String description);
+    
 }
