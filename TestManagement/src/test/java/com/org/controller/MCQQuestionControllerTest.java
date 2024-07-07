fda98e28961410fbcbec6a238bdfdbcbd5519ad2
Nikhil Shinde <nikhil.shinde@bnt-soft.com>
2024-07-07 16:40:06 +0530

    Changes in MCQQuestionControllerTest.java

--- TestManagement/src/test/java/com/org/controller/MCQQuestionControllerTest.java
+++ TestManagement/src/test/java/com/org/controller/MCQQuestionControllerTest.java
@@ -1,14 +1,15 @@
 package com.org.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,31 +19,36 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.mock.web.MockMultipartFile;
 
 import com.org.entities.Category;
 import com.org.entities.MCQQuestion;
 import com.org.entities.Subcategory;
-import com.org.services.MCQQuestionServiceImpl;
-import com.org.services.MCQService;
+import com.org.services.MCQQuestionService;
 import com.org.services.SubcategoryService;
+
 
 @ExtendWith(MockitoExtension.class)
 public class MCQQuestionControllerTest {
 
+	
     @Mock
-    private MCQService questionService;
+    private MCQQuestionService questionService;
     
     @Mock
     private SubcategoryService subcategoryService;
 
     @InjectMocks
-    private MCQQuestionController controller;
+    private MCQQuestionController mcqController;
     
     
     private Category category1;
     private Subcategory subcategory1;
     private Subcategory subcategory2;
+    
+    private MCQQuestion mockQuestion1;
+    private MCQQuestion mockQuestion2;
+    
 
     @BeforeEach
     public void setUp() {
@@ -50,55 +56,48 @@ public class MCQQuestionControllerTest {
         category1 = new Category(1L, "Java", "Core Java category");
         subcategory1 = new Subcategory(1L, category1, "Spring MVC", "Spring MVC subcategory");
         subcategory2 = new Subcategory(2L, category1, "Hibernate", "Hibernate subcategory");
+        mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
+        mockQuestion2 = new MCQQuestion(2L,subcategory2, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
+        
     }
 
     @Test
-    void getAllQuestions() {
+    void testGetAllQuestions() {
+    	
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
+    	ResponseEntity<MCQQuestion> addQuesion1 = mcqController.createQuestion(mockQuestion1);
+    	
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion2);
+    	ResponseEntity<MCQQuestion> addQuesion2 = mcqController.createQuestion(mockQuestion2);
         
-	 	MCQQuestion mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
-	 	MCQQuestion mockQuestion2 = new MCQQuestion(2L,subcategory2, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
-        List<MCQQuestion> expected = new ArrayList<>();
-        expected.add(mockQuestion1);
-        expected.add(mockQuestion2);
+	List<MCQQuestion> list=new ArrayList<MCQQuestion>();
+        list.add(mockQuestion1);
+        list.add(mockQuestion2);
 
-        when(questionService.getAllQuestions()).thenReturn(expected);
-
-        List<MCQQuestion> result = controller.getAllQuestions();
-
-        // Assertions
-        assertEquals(2, result.size());
-        assertEquals(1L, result.get(0).getId());
-        assertEquals(2L, result.get(1).getId());
+        when(questionService.getAllQuestions()).thenReturn(list);
+        ResponseEntity<List<MCQQuestion>> allQuestions = mcqController.getAllQuestions();
+        assertEquals(HttpStatus.OK, allQuestions.getStatusCode());
     }
 
     
     @Test
-    void getQuestionById() {
+    void testGetQuestionById() {
     	
-        // Mock data
-	 	MCQQuestion mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
+    	//Adding Mock Data
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
+        ResponseEntity<MCQQuestion> addQuestion1 = mcqController.createQuestion(mockQuestion1);
 
-        // Mock behavior
         when(questionService.getQuestionById(anyLong())).thenReturn(mockQuestion1);
-
-        // Call controller method
-        ResponseEntity<MCQQuestion> actual = controller.getQuestionById(1L);
-
-        // Assertions
+        ResponseEntity<MCQQuestion> actual = mcqController.getQuestionById(1L);
         assertEquals(HttpStatus.OK, actual.getStatusCode());
         assertEquals(1L, actual.getBody().getId());
     }
 
   
     @Test
-    void createQuestion() {
-       
-	 	MCQQuestion mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
-        ResponseEntity<MCQQuestion> mockResponseEntity = ResponseEntity.status(HttpStatus.CREATED).body(mockQuestion1);
-
-        when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
-
-        ResponseEntity<MCQQuestion> actual = controller.createQuestion(mockQuestion1);
+    void testCreateQuestion() {
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
+	ResponseEntity<MCQQuestion> actual = mcqController.createQuestion(mockQuestion1);
 
         assertEquals(HttpStatus.CREATED, actual.getStatusCode());
         assertEquals(1L, actual.getBody().getId());
@@ -106,31 +105,43 @@ public class MCQQuestionControllerTest {
 
  
     @Test
-    void updateQuestion() {
-	 	MCQQuestion mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
-        ResponseEntity<MCQQuestion> mockResponseEntity = ResponseEntity.ok().body(mockQuestion1);
+    void testUpdateQuestion() {
+	 	
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
+    	ResponseEntity<MCQQuestion> addQuestion = mcqController.createQuestion(mockQuestion1);
+    	
+    	//Updated Data
+        mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot Updated", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
 
         when(questionService.updateQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
-
-        ResponseEntity<MCQQuestion> actual = controller.updateQuestion(1L, mockQuestion1);
-
+        ResponseEntity<MCQQuestion> updateQuestion = mcqController.updateQuestion(1L, mockQuestion1);
+        
         // Assertions
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(1L, actual.getBody().getId());
+        assertEquals(HttpStatus.OK, updateQuestion.getStatusCode());
+        assertEquals(1L, updateQuestion.getBody().getId());
     }
 
     @Test
-    void deleteQuestion() {
+    void testDeleteQuestion() {
    
-	 	MCQQuestion mockQuestion1 = new MCQQuestion(1L,subcategory1, "SpringBoot", "In Spring Boot @RestController annotation is equivalent to", "@Controller and @PostMapping", "@Controller and @Component", "@Controller and @ResponseBody", "@Controller and @ResponseStatus", 3, -1);
-
+    	when(questionService.createQuestion(any(MCQQuestion.class))).thenReturn(mockQuestion1);
+    	ResponseEntity<MCQQuestion> addQuestion = mcqController.createQuestion(mockQuestion1);
+    	
         when(questionService.deleteQuestion(anyLong())).thenReturn(true);
-
-        ResponseEntity<HttpStatus> responseEntity1 = controller.deleteQuestion(1L);
-
+        ResponseEntity<HttpStatus> responseEntity1 = mcqController.deleteQuestion(1L);
         assertEquals(HttpStatus.OK, responseEntity1.getStatusCode());
         
     }
     
-    
+    @Test
+    public void testUploadBulkQuestion() throws Exception {
+        InputStream inputStream = new FileInputStream("D:\\QuestionBank.xlsx");
+        MockMultipartFile file = new MockMultipartFile("file", "QuestionBank.xlsx", "text/xlsx", inputStream);
+        List<MCQQuestion> list = new ArrayList<>();
+        
+        when(questionService.uploadQuestions(file)).thenReturn(list);
+       ResponseEntity<Object> uploadFile = mcqController.uploadFile(file);
+        assertEquals(HttpStatus.CREATED, uploadFile.getStatusCode());
+    }
+   
 }
